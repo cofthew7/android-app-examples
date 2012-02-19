96c0332999a3c4c2b25bf68ad07dc3adb96e0a97
Cofthew7 <cofthew7@gmail.com>
2012-02-19 16:43:56 +0800

    finish songs tab

--- MAD_HW2_TabView/src/hk/edu/uic/mad/hw/SongsActivity.java
+++ MAD_HW2_TabView/src/hk/edu/uic/mad/hw/SongsActivity.java
@@ -1,19 +1,35 @@
 package hk.edu.uic.mad.hw;
 
 import hk.edu.hk.mad.hw.R;
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SongsActivity extends Activity {
+public class SongsActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);	
         
-        TextView textview = new TextView(this);
-        textview.setText("This is the songs tab");
-        setContentView(textview);
+        String[] songs = getResources().getStringArray(R.array.songs_array);
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.songs, songs));
+
+        ListView lv = getListView();
+        lv.setTextFilterEnabled(true);
+
+        lv.setOnItemClickListener(new OnItemClickListener() {
+          public void onItemClick(AdapterView<?> parent, View view,
+              int position, long id) {
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                Toast.LENGTH_SHORT).show();
+          }
+        });
     }
 }
