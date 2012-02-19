d85b3b343cad106dc13c44ea8a14a6c6b1da0d2c
Cofthew7 <cofthew7@gmail.com>
2012-02-19 11:01:45 +0800

    import homework 2

---
+++ MAD_HW2_TabView/src/hk/edu/hk/mad/hw/MAD_HW2_TabViewActivity.java
@@ -0,0 +1,13 @@
+package hk.edu.hk.mad.hw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MAD_HW2_TabViewActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
