20185d90c86abb18fdc6dc36852ff2b30a42cc41
Cofthew7 <cofthew7@gmail.com>
2012-03-18 12:38:52 +0800

    init project

---
+++ MAD_HW5_GUANPOKER/src/hk/edu/uic/mad/hw/MainActivity.java
@@ -0,0 +1,13 @@
+package hk.edu.uic.mad.hw;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
